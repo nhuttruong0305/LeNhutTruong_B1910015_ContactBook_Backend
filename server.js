9e8648b76c03c8600648828fbfe2c20ef109fd26
Nhut Truong Le <truong.lnt123@gmail.com>
2023-02-21 16:16:45 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const app = require("./app");
 const config = require("./app/config");
-
+const MongoDB = require("./app/utils/mongodb.util")
 //start server
-const PORT = config.app.port;
-app.listen(PORT,() => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+async function startServer(){
+    try{
+        await MongoDB.connect(config.db.uri);
+        console.log("Connect to the database");
+
+        //định nghĩa hằng PORT là số hiệu cổng được cấu hình trong app/config/index.js
+        const PORT = config.app.port;
+
+        //Hàm được chạy khi server bắt đầu lắng nghe yêu cầu
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }catch(err){
+        console.log("Cannot connect to the database", err);
+        process.exit();
+    }
+}
+
+startServer();
